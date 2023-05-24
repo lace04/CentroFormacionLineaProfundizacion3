bfb2608e10a413604449f5a6fdfdccd595c7f01d
lace04 <luisace04@gmail.com>
2023-05-24 15:48:24 -0500

    initial commit

---
+++ AppWeb/Controllers/AlumnosAdminController .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppWeb.Models;
+using AppWeb.Models.Tablas;
+using AppWeb.Models.Modelos;
+
+namespace AppWeb.Controllers
+{
+    public class AlumnosAdminController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            List<TablaAlumnos> lst = null;
+            using (ColegioEntities db = new ColegioEntities())
+            {
+                           lst = (from d in db.Alumnos
+                           select new TablaAlumnos
+                           {
+                               Id = d.Id_Alumno,
+                               Nombre1 = d.Primer_Nombre,
+                               Nombre2 = d.Segundo_Nombre,
+                               Apellido1 = d.Primer_Apellido,
+                               Apellido2 = d.Segundo_Apellido,
+                               Identificacion = (decimal)d.Identificacion,
+                               Telefono = (decimal)d.Telefono,
+                               Usuario = d.Usuarios.Nombre_Usuario
+                           }).ToList();
+                
+            }
+            return View(lst);
+        }
+    }
+}
---
+++ AppWeb/Models/Tablas/TablaAlumnos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWeb.Models.Tablas
+{
+    public class TablaAlumnos
+    {
+        public int IdCurso { get; set; }
+        public int Id { get; set; }
+        public string Nombre1 { get; set; }
+        public string Nombre2 { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public decimal Identificacion { get; set; }
+        public decimal Telefono { get; set; }
+        public string Usuario { get; set; }
+       
+    }
+}
